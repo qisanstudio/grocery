6cfe1567a7a47adb99e1a5725ee0c360597274bb
qisan <yimiqisan@gmail.com>
2018-10-23 09:33:04 +0800

    fb

--- interview/interview0925.py
+++ interview/interview0925.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import random
+
+# 【题目】
+#     实现一个特殊的栈，在实现栈的基本功能的基础上，再实现返回栈中最小元素的操作。
+# 【要求】
+#     1. pop、push、get_min 操作的时间复杂度都是O(1)
+#     2. 设计的栈类型可以使用现成的栈结构
 
 
 class SelfMinValueStack(object):
-    '''
-    【题目】
-        实现一个特殊的栈，在实现栈的基本功能的基础上，再实现返回栈中最小元素的操作。
-    【要求】
-        1. pop、push、get_min 操作的时间复杂度都是O(1)
-        2. 设计的栈类型可以使用现成的栈结构
-    '''
 
     def __init__(self):
         self.min = -1
@@ -98,6 +96,13 @@ def test_self():
         s.pop()
         m = s.get_min()
         print s.data, m
+
+
+class ExpertMinValueStack():
+
+    def __init__(self):
+        # TODO min_stack 还可以记录data_stack的下标
+        pass
 
 
 def test_standard():
