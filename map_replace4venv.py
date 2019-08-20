d57a2d5894b5943f43998d451374190758df6b5b
qisan <yimiqisan@gmail.com>
2019-08-20 18:04:32 +0800

    add map_replace4venv

--- map_replace4venv.py
+++ map_replace4venv.py
@@ -1,20 +1,19 @@
+#!/root/.virtualenvs/venv/bin/python2
 # -*- coding: utf-8 -*-
 
 import os
-import click
+import sys
 from shutil import copyfile
 
 ORIGIN_SUFFIX = '.origin'
 
+# bin/postactivate file
+# python /root/.virtualenvs/venv/bin/map_replace.py replace /export/local /export/sure
+# bin/postdeactivate file
+# python /root/.virtualenvs/venv/bin/map_replace.py recover /export/sure
 
-@click.group()
-def cli():
-    pass
 
 
-@click.command()
-@click.option('--source', default='source dir')
-@click.option('--target', default='target dir')
 def replace(source, target):
     assert os.path.isabs(source) and os.path.isdir(source)
     assert os.path.isabs(target) and os.path.isdir(target)
@@ -32,8 +31,6 @@ def replace(source, target):
     print "origin file rename to xxx%s" % ORIGIN_SUFFIX
 
 
-@click.command()
-@click.option('--target', default='target dir')
 def recover(target):
     assert os.path.isabs(target) and os.path.isdir(target)
     for root, dirs, files in os.walk(target, topdown=False):
@@ -43,21 +40,14 @@ def recover(target):
             target_origin_file_path = '%s/%s' % (root, f)
             target_file_path = target_origin_file_path.replace(ORIGIN_SUFFIX, '')
             copyfile(target_origin_file_path, target_file_path)
+            print "%s recoverd" % target_file_path
             os.remove(target_origin_file_path)
 
 
-@click.command()
-@click.option('--p', default='target dir')
-def t(p):
-    print os.path.isabs(p)
-    print os.path.isdir(p)
-
-
-
-cli.add_command(replace)
-cli.add_command(recover)
-cli.add_command(t)
-
-
 if __name__ == '__main__':
-    cli()
+    if sys.argv[1].lower() == 'replace':
+        replace(sys.argv[2], sys.argv[3])
+    elif sys.argv[1].lower() == 'recover':
+        recover(sys.argv[2])
+    else:
+        print 'replace | recover'
